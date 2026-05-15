f38c60c0f6dea075e287ab04a245189f3027470e
Axel <areoyops18@gmail.com>
2026-05-15 10:50:24 +0200

    Model-JavaClass

--- Booksland/src/main/java/com/svalero/booksland/model/Book.java
+++ Booksland/src/main/java/com/svalero/booksland/model/Book.java
@@ -1,4 +1,14 @@
 package com.svalero.booksland.model;
 
+import lombok.Data;
+
+@Data
 public class Book {
+
+    private int id;
+    private String title;
+    private String genre;
+    private int pages;
+    private String publisher;
+
 }
