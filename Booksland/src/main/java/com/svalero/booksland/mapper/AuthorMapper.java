4e149d9943506a2574a2b5b168bb39d9efd1161d
Axel <areoyops18@gmail.com>
2026-05-15 11:56:14 +0200

    Mapper

--- Booksland/src/main/java/com/svalero/booksland/mapper/AuthorMapper.java
+++ Booksland/src/main/java/com/svalero/booksland/mapper/AuthorMapper.java
@@ -1,4 +1,23 @@
 package com.svalero.booksland.mapper;
 
-public class AuthorMapper {
+import com.svalero.booksland.model.Author;
+import com.svalero.booksland.model.Book;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthorMapper implements RowMapper<Author> {
+    @Override
+    public Author map(ResultSet rs, StatementContext ctx) throws SQLException {
+        Author author = new Author();
+        author.setId(rs.getInt("id"));
+        author.setName(rs.getString("name"));
+        author.setLastName(rs.getString("last_name"));
+        author.setNationality(rs.getString("nationality"));
+        author.setBiography(rs.getString("biography"));
+
+        return author;
+    }
 }
--- Booksland/src/main/java/com/svalero/booksland/mapper/BookMapper.java
+++ Booksland/src/main/java/com/svalero/booksland/mapper/BookMapper.java
@@ -1,4 +1,23 @@
 package com.svalero.booksland.mapper;
 
-public class BookMapper {
+import com.svalero.booksland.model.Book;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookMapper implements RowMapper<Book> {
+    @Override
+    public Book map(ResultSet rs, StatementContext ctx) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getInt("id"));
+        book.setTitle(rs.getString("title"));
+        book.setGenre(rs.getString("genre"));
+        book.setPages(rs.getInt("pages"));
+        book.setPublisher(rs.getString("publisher"));
+        book.setIdAuthor(rs.getInt("id_author"));
+
+        return book;
+    }
 }
